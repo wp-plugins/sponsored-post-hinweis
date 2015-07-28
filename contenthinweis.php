c0bca78c56b50d813ee5f2df1af78a8f8b6f3ae7
Goerni <Goerni@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-28 10:30:36 +0000

    tagging versino 1.5
    git-svn-id: https://plugins.svn.wordpress.org/sponsored-post-hinweis/tags/1.5@1208142 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contenthinweis.php
+++ contenthinweis.php
@@ -1,22 +1,51 @@
 <?php
 /**
  * @package Sponsored_Post_Hinweis
- * @version 1.2
+ * @version 1.5
  */
 /*
 Plugin Name: Sponsored Post Hinweis
 Plugin URI: http://wordpress.org/plugins/sponsored-post-hinweis/
 Description: Dieses PlugIn setzt (default) einfach ein [Sponsored Post] vor alle Artikel. Über ein benutzerdefiniertes Feld mit dem Namen "hinweis" kann als Wert ein beliebiger Text hinterlegt werden. Beim hinterlegten Wert "no" kann der Hinweis für den einzelnen Artikel ausgeblendet werden.
 Author: Gernot Gawlik
-Version: 1.2
+Version: 1.5
 Author URI: http://www.krick-interactive.com
 */
 
 // Die Funktion die den Hinweis vor den Content stellen soll
 function contenthinweis($content)
 {
+$option_string = get_option('sponsoredFunktionen');
+$option = array();
+$option = json_decode($option_string, true);
+
+//(Standard) Variablen deklarieren
+$sponsoredhinweistext = 'Sponsored Post';
+$praefixtext ="[";
+$suffixtext ="]";
+$leerzeichen = " ";
+
+
 	//Variable für Hinweis deklarieren
-	$sponsoredhinweis = '[Sponsored Post] ';
+	if (isset($option['standardtext'])){
+		$sponsoredhinweistext = $option['standardtext'];
+	}else{
+		$sponsoredhinweistext = 'Sponsored Post';
+	}
+
+	//Präfix
+	if (isset($option['praefixtext'])){ 
+		$praefixtext = $option['praefixtext'];
+	}
+
+	//Suffix
+	if (isset($option['suffixtext'])){ 
+		$suffixtext = $option['suffixtext'];
+	}
+
+//Komplett
+$sponsoredhinweis = $praefixtext.$sponsoredhinweistext.$suffixtext.$leerzeichen;
+	
 	//Absatzkorrektur
 	$search = "<p>";
 
@@ -30,7 +59,7 @@ function contenthinweis($content)
 				if ( $value == 'no'){
 					$sponsoredhinweis = "";
 				}else{
-					$sponsoredhinweis = "$value";
+					$sponsoredhinweis = $value." ";
 				}
 			}
 		}
@@ -51,4 +80,64 @@ add_filter(
 	'the_content', //Name des Hooks
 	'contenthinweis' //Name der Funktion
 );
+?>
+
+<?php
+add_action('admin_menu', 'sponsoredFunktionen_menu');
+function sponsoredFunktionen_menu() {
+	add_options_page('Sponsored Post Hinweis Optionen', 'Sponsored Post Hinweis Optionen', 'manage_options', 'sponsoredFunktionen', 'sponsoredFunktionen_options');
+}
+
+function sponsoredFunktionen_options () {
+	$option_name = 'sponsoredFunktionen';
+	if (!current_user_can('manage_options')) {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+	if( isset($_POST['standardtext'])) {
+		$option = array();
+		$option['standardtext'] = esc_html($_POST['standardtext']);
+		$option['praefixtext'] = esc_html($_POST['praefixtext']);
+		$option['suffixtext'] = esc_html($_POST['suffixtext']);
+		update_option($option_name, json_encode($option));
+		$outputa .= '<div class="updated"><p><strong>'.__('Einstellungen gespeichert.', 'menu' ).'</strong></p></div>';
+	}
+	$option = array();
+	$option_string = get_option($option_name);
+	if ($option_string===false) {
+		$option = array();
+		$option['standardtext'] = array('standardtext'=>true);
+		$option['praefixtext'] = array('praefixtext'=>true);
+		$option['suffixtext'] = array('suffixtext'=>true);
+		$option_string = get_option($option_name);
+	}
+	$option = json_decode($option_string, true);
+	$outputa .= '
+	<div class="wrap">
+		<h2>'.__( 'Sponsored Post Hinweis Optionen', 'menu' ).'</h2>
+		<form name="form1" method="post" action="">
+		<table>
+		<tr><td valign="top"><b>'.__("Standardtext", 'menu' ).':</b></td>
+		<td style="padding-bottom:20px;">
+		<input type="text" name="standardtext" value="'.stripslashes($option['standardtext']).'" size="100"><br />
+		<span class="description">z.B.: '.htmlentities('Sponsored Post').'</span>
+		</td></tr>
+		<tr><td valign="top"><b>'.__("Praefix", 'menu' ).':</b></td>
+		<td style="padding-bottom:20px;">
+		<input type="text" name="praefixtext" value="'.stripslashes($option['praefixtext']).'" size="100"><br />
+		<span class="description">z.B.: '.htmlentities('[').'</span>
+		</td></tr>
+		<tr><td valign="top"><b>'.__("Suffix", 'menu' ).':</b></td>
+		<td style="padding-bottom:20px;">
+		<input type="text" name="suffixtext" value="'.stripslashes($option['suffixtext']).'" size="100"><br />
+		<span class="description">z.B.: '.htmlentities(']').'</span>
+		</td></tr>
+		</table>
+		<hr />
+		<p class="submit">
+			<input type="submit" name="Submit" class="button-primary" value="'.esc_attr('Speichern').'" />
+		</p>
+		</form>
+	</div>';
+	echo $outputa; 
+}
 ?>
